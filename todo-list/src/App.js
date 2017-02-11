b5f8678ab04156ec7ff5c302605fe7b0cbf7a061
Osbert Orr <osbert.orr@digitalinsight.com>
2017-02-11 07:37:13 -0800

    Add local storage

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,58 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import AddItem from './AddItem';
 import List from './List';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {list: this.getList()};
-    this.addItem = this.addItem.bind(this);
-    this.removeItem = this.removeItem.bind(this);
-    this.toggleCheck = this.toggleCheck.bind(this);
-    this.update = this.update.bind(this);
-  }
-
-  getList() {
-    var toDoList;
-
-    if(!toDoList) {
-      toDoList = [];
+    constructor(props) {
+        super(props);
+        this.state = {list: this.getList()};
+        this.addItem = this.addItem.bind(this);
+        this.removeItem = this.removeItem.bind(this);
+        this.toggleCheck = this.toggleCheck.bind(this);
+        this.update = this.update.bind(this);
     }
 
-    return toDoList;
-  }
+    getList() {
+        var toDoList = JSON.parse(window.localStorage.getItem('toDoList'));
 
-  addItem(itemName) {
-    this.state.list.push({itemName, checked: false});
-    this.update();
-  }
+        if (!toDoList) {
+            toDoList = [];
+        }
 
-  toggleCheck(index) {
-    const item = this.state.list[index];
-    item.checked = !item.checked;
-    this.update();
-  }
+        return toDoList;
+    }
 
-  removeItem(index) {
-    this.state.list.splice(index,1);
-    this.update();
-  }
+    addItem(itemName) {
+        this.state.list.push({itemName, checked: false});
+        this.update();
+    }
 
-  update() {
-    this.setState({list: this.state.list});
-    console.log(this.state.list);
-  }
+    toggleCheck(index) {
+        const item = this.state.list[index];
+        item.checked = !item.checked;
+        this.update();
+    }
 
-  render() {
-    return (
-        <div>
-          <h1>To Do List</h1>
-          <AddItem addItem={this.addItem}/>
-          <List list={this.state.list} removeItem={this.removeItem} toggleCheck={this.toggleCheck}/>
-        </div>
-    );
-  }
+    removeItem(index) {
+        this.state.list.splice(index, 1);
+        this.update();
+    }
+
+    update() {
+        this.setState({list: this.state.list});
+        window.localStorage.setItem('toDoList', JSON.stringify(this.state.list));
+    }
+
+    componentDidMount() {
+        window.addEventListener('storage', () => {
+            this.setState({list: this.getList()});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>To Do List</h1>
+                <AddItem addItem={this.addItem}/>
+                <List list={this.state.list} removeItem={this.removeItem} toggleCheck={this.toggleCheck}/>
+            </div>
+        );
+    }
 }
 
 export default App;
